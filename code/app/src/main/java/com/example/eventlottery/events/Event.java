b03a39f64328b704c2b4b524f3fdbfd788341542
ben <78438071+bolson13@users.noreply.github.com>
2025-10-29 21:02:34 -0600

    Updated Event class to support images and QR codes

--- code/app/src/main/java/com/example/eventlottery/events/Event.java
+++ code/app/src/main/java/com/example/eventlottery/events/Event.java
@@ -1,8 +1,10 @@
 package com.example.eventlottery.events;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Event {
@@ -10,8 +12,11 @@ public class Event {
     private String name;
     private String description;
     private String location; // Could also be a HashMap so we can easily grab individual location info (street, city, etc..)
+    private String organizer;
+    private int image;
     private Date startTime;
     private Date endTime;
+    private QRCode qrCode;
 
     @NonNull
     private String generateUUID() {
@@ -19,22 +24,28 @@ public class Event {
         return uuid.toString();
     }
 
-    public Event(String name, String description, String location, Date startTime, Date endTime) { // For new events (not yet in database)
+    public Event(String name, String description, String location, String organizer, int image, Date startTime, Date endTime) { // For new events (not yet in database)
         this.id = generateUUID();
         this.name = name;
         this.description = description;
         this.location = location;
+        this.organizer = organizer;
+        this.image = image;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.qrCode = new QRCode(this.id);
     }
 
-    public Event(String id, String name, String description, String location, Date startTime, Date endTime) { // For pre-existing events
+    public Event(String id, String name, String description, String location, String organizer, int image, Date startTime, Date endTime) { // For pre-existing events
         this.id = id;
         this.name = name;
         this.description = description;
         this.location = location;
+        this.organizer = organizer;
+        this.image = image;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.qrCode = new QRCode(this.id);
     }
 
     public String getId() {
@@ -65,6 +76,22 @@ public class Event {
         this.location = location;
     }
 
+    public String getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(String organizer) {
+        this.organizer = organizer;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
     public Date getStartTime() {
         return startTime;
     }
@@ -79,5 +106,22 @@ public class Event {
 
     public void setEndTime(Date endTime) {
         this.endTime = endTime;
+    }
+
+    public QRCode getQrCode() {
+        return qrCode;
+    }
+
+    public void setQrCode(QRCode qrCode) {
+        this.qrCode = qrCode;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (obj instanceof Event) {
+            Event otherEvent = (Event) obj;
+            return Objects.equals(this.id, otherEvent.getId());
+        }
+        return false;
     }
 }
