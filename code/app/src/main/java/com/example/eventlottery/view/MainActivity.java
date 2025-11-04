f50251bf1b4af17cb4f92c5981ac8d3536ff9969
Harjot Singh <Harjotmatharu16@gmail.com>
2025-11-03 23:52:39 -0700

    save current changes

--- code/app/src/main/java/com/example/eventlottery/view/MainActivity.java
+++ code/app/src/main/java/com/example/eventlottery/view/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         DEVICE_ID = getDeviceId(this);
         // Load user data here using DEVICE_ID
 
