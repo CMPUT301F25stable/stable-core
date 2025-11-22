409b0f097bd0e0355b3da58ec8c6ab73d539c80a
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-11-21 22:02:07 -0700

    Add dependencies for geolocation

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -50,6 +50,14 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.fragment:fragment:1.8.3")
 
+    // --- Geolocation ---
+    implementation("com.google.android.gms:play-services-location:17.0.0")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+
+
+
     // --- Firebase ---
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.google.firebase:firebase-firestore")
