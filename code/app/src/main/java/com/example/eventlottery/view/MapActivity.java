e64430e1418a90ef7807f5336445ce7e8d84514c
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-11-22 22:20:30 -0700

    Added dependencies & files to setup maps user story

---
+++ code/app/src/main/java/com/example/eventlottery/view/MapActivity.java
@@ -0,0 +1,46 @@
+package com.example.eventlottery.view;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.eventlottery.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * The activity for displaying where entrants have joined your event as an organizer.
+ * References: https://www.geeksforgeeks.org/android/google-maps-in-android/
+ * https://developers.google.com/maps/documentation/android-sdk/get-api-key?setupProd=enable
+ */
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private SupportMapFragment mapFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_map);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng position = new LatLng(-34, 151);  // FILLER: Change later
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+    }
+}
